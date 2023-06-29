0356d6b894ededa97ddc2559d9730f02dc9e12c8
hien dep trai <nguyenvahiencntt3@gmail.com>
2023-06-29 15:25:51 +0700

    test husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,11 +68,7 @@ export class AppComponent implements OnInit {
   }
 
   onScroll() {
-    this.isLoading$.next(true)
-    this.page++
-    fetchPassenger(this.http, this.page)
-      .pipe(map((res) => res.data))
-      .subscribe((res) => {
+    this.isLoading$.next(true);this.page++;fetchPassenger(this.http, this.page).pipe(map((res) => res.data)).subscribe((res) => {
         this.passengers = [...this.passengers, ...res]
         this.isLoading$.next(false)
       })
